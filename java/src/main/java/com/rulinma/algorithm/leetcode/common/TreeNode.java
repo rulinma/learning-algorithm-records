a822c5ac37a2ba09a17d954d1c237b639b3db927
rulinma <rulinma@gmail.com>
2022-06-19 18:27:52 +0800

    添加TreeNode

---
+++ java/src/main/java/com/rulinma/algorithm/leetcode/common/TreeNode.java
@@ -0,0 +1,30 @@
+package com.rulinma.algorithm.leetcode.common;
+
+/**
+ * @author 马如林
+ * @Data 2022/6/19 18:26
+ */
+
+
+/**
+ * Definition for a binary tree node.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
