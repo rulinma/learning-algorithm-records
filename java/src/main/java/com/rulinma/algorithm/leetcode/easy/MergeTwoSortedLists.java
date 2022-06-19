86cec918e0b1a1cdfd5bfefc2b22f85992f79238
rulinma <rulinma@gmail.com>
2022-06-19 18:12:35 +0800

    合并2个有序列表

--- java/src/main/java/com/rulinma/algorithm/leetcode/easy/MergeTwoSortedLists.java
+++ java/src/main/java/com/rulinma/algorithm/leetcode/easy/MergeTwoSortedLists.java
@@ -10,12 +10,17 @@ import com.rulinma.algorithm.leetcode.common.ListNode;
  * @author 马如林
  * @Data 2022/6/19 17:46
  */
-public class mergeTwoSortedLists {
+public class MergeTwoSortedLists {
 
     /**
-     * 1. 创建一个新链表
-     * 2. 比较1和2的头结点，选择小的添加到新链表，1或者2指针前进直到为空
-     * 3. 为空则把未遍历完的链表指针添加到新链表尾部即可
+     * 首先，设定一个虚拟节点 dummy 用来存储结果，循环对比 L1 和 L2 节点上的数字，通过调整 p节点的 next 指针来调整 dummy 的结果。
+     * 如果 L1 当前位置的值小于等于 L2 ，我们就把  L1 的值接在  dummy 节点的后面同时将  L1 指针往后移一个
+     * 如果 L2 当前位置的值小于 L2 ，我们就把  L2 的值接在  p 节点的后面同时将  L2 指针往后移一个
+     * 不管我们将哪一个元素接在了 p 节点后面，都需要向后移一个元素
+     * 重复以上过程，直到  L1 或者  L2 指向了 null
+     * 在循环终止的时候，  L1 和  L2 至多有一个是非空的。由于输入的两个链表都是有序的，所以不管哪个链表是非空的，它包含的所有元素都比前面已经合并链表中的所有元素都要大。这意味着我们只需要简单地将非空链表接在合并链表的后面，并返回合并链表。
+     * <p>
+     * 算法空间复杂度 O1，时间复杂度 O（n + m）
      *
      * @param list1
      * @param list2
@@ -29,6 +34,7 @@ public class mergeTwoSortedLists {
         // 3. 返回新链表未head节点的指针
 
         ListNode result = new ListNode(-1);
+        // 添加一个新链表当前指针位置的变量
         ListNode head = result;
 
         while (list1 != null && list2 != null) {
