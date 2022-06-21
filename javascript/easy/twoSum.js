c294c613a83c8b335efcdbd52bea1d2be38b70bb
rulinma <rulinma@gmail.com>
2022-06-21 15:48:40 +0800

    javascript two sum

--- javascript/easy/twoSum.js
+++ javascript/easy/twoSum.js
@@ -12,7 +12,30 @@ console.log("two sum")
  * @param {number} target
  * @return {number[]}
  */
+
 var twoSum = function(nums, target) {
+// map处理
+// 1. map.put(num, i)
+// 2. 遍历nums if(target - num) exist map中则返回 
+    const map = new Map();
+    // let i = 0;
+    // nums.forEach(item => { 
+    //     map.set(item, i++);
+    // })
+    // debugger
+    map.set(nums[0], 0);
+    for (let j = 1; j < nums.length; j++) { 
+        if (map.has(target - nums[j])) {
+            return [map.get(target - nums[j]), j ]
+        } else { 
+            map.set(nums[j], j); 
+        }
+    }
+    
+    return []
+};
+
+var twoSum0 = function(nums, target) {
     for (let i = 0; i < nums.length - 1; i++ ) { 
         for (let j = i + 1; j < nums.length; j++) { 
             if ((nums[i] + nums[j]) == target) { 
@@ -24,8 +47,8 @@ var twoSum = function(nums, target) {
     return []
 };
 
-const nums = [1, 2, 3];
-const target = 5;
+const nums = [3, 2, 4];
+const target = 6;
 
 let result = twoSum(nums, target)
 console.log("result ", result)
