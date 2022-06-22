4d1fd6e61bce362561171e5f09147fd43d4c3852
rulinma <rulinma@gmail.com>
2022-06-22 10:32:39 +0800

    添加readme

--- javascript/easy/reverseLinkedList.js
+++ javascript/easy/reverseLinkedList.js
@@ -1,4 +1,14 @@
 /**
+ * 206. 反转链表
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ * <p>
+ * https://leetcode.cn/problems/reverse-linked-list/
+ *
+ * @author 马如林
+ * @Data 2022/6/21 22:40
+ */
+
+/**
  * @param {ListNode} head
  * @return {ListNode}
  */
