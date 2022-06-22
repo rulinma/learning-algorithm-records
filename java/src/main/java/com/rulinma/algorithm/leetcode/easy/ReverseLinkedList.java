f3cf411304a84c0acd7e68f5d9979cebdc5ee886
rulinma <rulinma@gmail.com>
2022-06-22 10:03:51 +0800

    链表反转

--- java/src/main/java/com/rulinma/algorithm/leetcode/easy/ReverseLinkedList.java
+++ java/src/main/java/com/rulinma/algorithm/leetcode/easy/ReverseLinkedList.java
@@ -9,6 +9,25 @@ import com.rulinma.algorithm.leetcode.common.ListNode;
 public class ReverseLinkedList {
 
     public ListNode reverseList(ListNode head) {
+        ListNode cur = null;
+        ListNode pre = head;
+        // cur pre 结构
+        while (pre != null) {
+            // 下面顺便一个不能错！
+            // 保存下一个节点
+            ListNode next = pre.next;
+            // 反向指向
+            pre.next = cur;
+            // 后移cur
+            cur = pre;
+            // 后移pre
+            pre = next;
+        }
+
+        return cur;
+    }
+
+    public ListNode reverseList1(ListNode head) {
         ListNode pre = null;
         ListNode cur = head;
         // 1.  pre current next 指针转换
