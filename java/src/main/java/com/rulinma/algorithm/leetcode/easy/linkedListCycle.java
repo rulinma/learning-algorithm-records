45ce5b95e30fb33a98ee19725d54165de0988d78
rulinma <rulinma@gmail.com>
2022-06-22 17:12:17 +0800

    环形链表

---
+++ java/src/main/java/com/rulinma/algorithm/leetcode/easy/linkedListCycle.java
@@ -0,0 +1,34 @@
+package com.rulinma.algorithm.leetcode.easy;
+
+import com.rulinma.algorithm.leetcode.common.ListNode;
+
+/**
+ * 141. 环形链表
+ * 给你一个链表的头节点 head ，判断链表中是否有环。
+ * <p>
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，
+ * 评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+ * 注意：pos 不作为参数进行传递 。仅仅是为了标识链表的实际情况。
+ * <p>
+ * 链接：https://leetcode.cn/problems/linked-list-cycle
+ *
+ * @author 马如林
+ * @Data 2022/6/22 17:02
+ */
+public class linkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        // slow fast
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
