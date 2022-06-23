7d19b03bf97449369d706d642d2328072458e722
rulinma <rulinma@gmail.com>
2022-06-23 19:45:59 +0800

    丢失的数字

--- java/src/main/java/com/rulinma/algorithm/leetcode/easy/MissingNumber.java
+++ java/src/main/java/com/rulinma/algorithm/leetcode/easy/MissingNumber.java
@@ -27,7 +27,6 @@ public class MissingNumber {
 
         // 给定一个包含 [0, n] 中 n 个数的数组 nums ，找出 [0, n] 这个范围内没有出现在数组中的那个数。
         Set<Integer> set = new HashSet<>();
-
         for (int x : nums) {
             set.add(x);
         }
