0a439ba4ac42557e0e7144de5dacced53444046b
rulinma <rulinma@gmail.com>
2022-06-24 12:56:41 +0800

    轮转数组

---
+++ java/src/main/java/com/rulinma/algorithm/leetcode/middle/RotateArray.java
@@ -0,0 +1,44 @@
+package com.rulinma.algorithm.leetcode.middle;
+
+import java.util.Arrays;
+
+/**
+ * @author 马如林
+ * @Data 2022/6/24 12:45
+ */
+public class RotateArray {
+    public static void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        if (k > nums.length) {
+            return;
+        }
+        int len = nums.length;
+        int index = len - k;
+
+        int[] result = new int[nums.length];
+        // k个
+        int i = 0;
+        while (index < len) {
+            result[i++] = nums[index++];
+        }
+
+        // 从0开始到k-1个
+        int j = 0;
+        while (j < len - k) {
+            result[i++] = nums[j++];
+        }
+
+        nums = result;
+        System.out.println(Arrays.toString(nums));
+
+        System.arraycopy(result, 0, nums, 0, len);
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+
+        rotate(nums, k);
+    }
+}
