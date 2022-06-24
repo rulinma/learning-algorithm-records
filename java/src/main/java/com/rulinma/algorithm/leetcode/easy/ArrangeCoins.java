51a0f229c7639166bcb8bc253519648c44ad18d6
rulinma <rulinma@gmail.com>
2022-06-24 11:44:35 +0800

    排列硬币

--- java/src/main/java/com/rulinma/algorithm/leetcode/easy/ArrangeCoins.java
+++ java/src/main/java/com/rulinma/algorithm/leetcode/easy/ArrangeCoins.java
@@ -1,6 +1,12 @@
 package com.rulinma.algorithm.leetcode.easy;
 
 /**
+ * 441. 排列硬币
+ * 你总共有 n 枚硬币，并计划将它们按阶梯状排列。对于一个由 k 行组成的阶梯，其第 i 行必须正好有 i 枚硬币。阶梯的最后一行 可能 是不完整的。
+ * 给你一个数字 n ，计算并返回可形成 完整阶梯行 的总行数。
+ * <p>
+ * https://leetcode.cn/problems/arranging-coins
+ *
  * @author 马如林
  * @Data 2022/6/24 11:26
  */
@@ -21,7 +27,7 @@ public class ArrangeCoins {
     }
 
     public long kCoins(int k) {
-        return ( ((long)k * ((long)k + 1))) / 2;
+        return (((long) k * ((long) k + 1))) / 2;
     }
 
     public static void main(String[] args) {
