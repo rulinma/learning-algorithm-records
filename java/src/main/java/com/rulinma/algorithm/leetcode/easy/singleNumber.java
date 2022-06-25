7d450e604bf652293ff86d947a00f7b2dc71be31
rulinma <rulinma@gmail.com>
2022-06-25 18:47:02 +0800

    只出现一次的数字

---
+++ java/src/main/java/com/rulinma/algorithm/leetcode/easy/singleNumber.java
@@ -0,0 +1,34 @@
+package com.rulinma.algorithm.leetcode.easy;
+
+/**
+ * 136. 只出现一次的数字
+ * <p>
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ * <p>
+ * 说明：
+ * <p>
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [2,2,1]
+ * 输出: 1
+ * 示例 2:
+ * <p>
+ * 输入: [4,1,2,1,2]
+ * 输出: 4
+ * <p>
+ * https://leetcode.cn/problems/single-number
+ *
+ * @author 马如林
+ * @Data 2022/6/25 18:46
+ */
+public class singleNumber {
+    public int singleNumber(int[] nums) {
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            result = result ^ nums[i];
+        }
+        return result;
+    }
+}
