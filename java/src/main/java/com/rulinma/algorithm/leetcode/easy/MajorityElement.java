2b19e43badf4c9c7c648412333c059942436378f
rulinma <rulinma@gmail.com>
2022-06-26 12:55:28 +0800

    多数元素

--- java/src/main/java/com/rulinma/algorithm/leetcode/easy/MajorityElement.java
+++ java/src/main/java/com/rulinma/algorithm/leetcode/easy/MajorityElement.java
@@ -3,6 +3,23 @@ package com.rulinma.algorithm.leetcode.easy;
 import java.util.Arrays;
 
 /**
+ * 169. 多数元素
+ * <p>
+ * 给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+ * <p>
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [3,2,3]
+ * 输出：3
+ * 示例 2：
+ * <p>
+ * 输入：nums = [2,2,1,1,1,2,2]
+ * 输出：2
+ * <p>
+ * https://leetcode.cn/problems/majority-element
+ *
  * @author 马如林
  * @Data 2022/6/26 12:54
  */
