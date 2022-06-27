944bd548c6dbcdb92e1c4bbcc9d25011ee9ae353
rulinma <rulinma@gmail.com>
2022-06-27 19:24:06 +0800

    最小栈

---
+++ java/src/main/java/com/rulinma/algorithm/leetcode/easy/MinStack.java
@@ -0,0 +1,53 @@
+package com.rulinma.algorithm.leetcode.easy;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 155. 最小栈
+ * <p>
+ * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+ * <p>
+ * 实现 MinStack 类:
+ * <p>
+ * MinStack() 初始化堆栈对象。
+ * void push(int val) 将元素val推入堆栈。
+ * void pop() 删除堆栈顶部的元素。
+ * int top() 获取堆栈顶部的元素。
+ * int getMin() 获取堆栈中的最小元素。
+ * <p>
+ * https://leetcode.cn/problems/min-stack
+ *
+ * @author 马如林
+ * @Data 2022/6/27 19:21
+ */
+public class MinStack {
+
+    Deque<Integer> xStack;
+    Deque<Integer> minStack;
+
+    public MinStack() {
+        xStack = new LinkedList<Integer>();
+        minStack = new LinkedList<Integer>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int x) {
+        xStack.push(x);
+        minStack.push(Math.min(minStack.peek(), x));
+    }
+
+    public void pop() {
+        xStack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return xStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+}
