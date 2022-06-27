44bb3d3585a5f06e4c1ad66ed442b8a2597e67f2
rulinma <rulinma@gmail.com>
2022-06-27 19:52:36 +0800

    Excel 表列序号

---
+++ java/src/main/java/com/rulinma/algorithm/leetcode/easy/ExcelSheetColumnNumber.java
@@ -0,0 +1,53 @@
+package com.rulinma.algorithm.leetcode.easy;
+
+/**
+ * 171. Excel 表列序号
+ * 给你一个字符串 columnTitle ，表示 Excel 表格中的列名称。返回 该列名称对应的列序号 。
+ * <p>
+ * 例如：
+ * <p>
+ * A -> 1
+ * B -> 2
+ * C -> 3
+ * ...
+ * Z -> 26
+ * AA -> 27
+ * AB -> 28
+ * <p>
+ * https://leetcode.cn/problems/excel-sheet-column-number
+ *
+ * @author 马如林
+ * @Data 2022/6/27 19:42
+ */
+public class ExcelSheetColumnNumber {
+
+    public int titleToNumber(String columnTitle) {
+        int result = 0;
+        // 1. 26进制
+        int len = columnTitle.length();
+        int i = len - 1;
+        int j = 0;
+        while (i >= 0) {
+            int number = 1;
+            int count = 0;
+            if (j == 0) {
+                number = 1;
+            } else {
+                while (count++ < j) {
+                    number = number * 26;
+                }
+            }
+            result += number * (columnTitle.charAt(i) - 'A' + 1);
+            i--;
+            j++;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ExcelSheetColumnNumber excelSheetColumnNumber = new ExcelSheetColumnNumber();
+        int result = excelSheetColumnNumber.titleToNumber("ZY");
+        System.out.println(result);
+    }
+}
