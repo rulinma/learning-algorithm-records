da2192f20afd06f7bfd7aed74d7a918f22425ba2
rulinma <rulinma@gmail.com>
2022-06-27 19:53:57 +0800

    Excel 表列序号

--- java/src/main/java/com/rulinma/algorithm/leetcode/easy/ExcelSheetColumnNumber.java
+++ java/src/main/java/com/rulinma/algorithm/leetcode/easy/ExcelSheetColumnNumber.java
@@ -22,6 +22,17 @@ package com.rulinma.algorithm.leetcode.easy;
 public class ExcelSheetColumnNumber {
 
     public int titleToNumber(String columnTitle) {
+        int number = 0;
+        int multiple = 1;
+        for (int i = columnTitle.length() - 1; i >= 0; i--) {
+            int k = columnTitle.charAt(i) - 'A' + 1;
+            number += k * multiple;
+            multiple *= 26;
+        }
+        return number;
+    }
+
+    public int titleToNumber0(String columnTitle) {
         int result = 0;
         // 1. 26进制
         int len = columnTitle.length();
