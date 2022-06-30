1586caca244ab3124dde58935ea261d5e22eae3e
rulinma <rulinma@gmail.com>
2022-06-30 16:56:44 +0800

    矩阵置零

--- java/src/main/java/com/rulinma/algorithm/leetcode/middle/SetMatrixZeroes.java
+++ java/src/main/java/com/rulinma/algorithm/leetcode/middle/SetMatrixZeroes.java
@@ -15,14 +15,35 @@ import java.util.List;
  * 输入：matrix = [[1,1,1],[1,0,1],[1,1,1]]
  * 输出：[[1,0,1],[0,0,0],[1,0,1]]
  * <p>
-
+ * <p>
  * https://leetcode.cn/problems/set-matrix-zeroes
  *
  * @author 马如林
  * @Data 2022/6/30 16:46
  */
 public class SetMatrixZeroes {
+
     public void setZeroes(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = col[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public void setZeroes1(int[][] matrix) {
         int row = matrix.length;
         int col = matrix[0].length;
         List<Integer> rows = new ArrayList<>();
