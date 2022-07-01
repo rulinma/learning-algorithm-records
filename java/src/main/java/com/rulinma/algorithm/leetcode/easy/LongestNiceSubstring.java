44770e1aae569194d6fac299cbfdc6fefd92ab09
rulinma <rulinma@gmail.com>
2022-07-01 10:50:35 +0800

    最长的美好子字符串

--- java/src/main/java/com/rulinma/algorithm/leetcode/easy/LongestNiceSubstring.java
+++ java/src/main/java/com/rulinma/algorithm/leetcode/easy/LongestNiceSubstring.java
@@ -36,6 +36,28 @@ public class LongestNiceSubstring {
 
     public String longestNiceSubstring(String s) {
         int n = s.length();
+        int maxPos = 0;
+        int maxLen = 0;
+        for (int i = 0; i < n; ++i) {
+            int lower = 0;
+            int upper = 0;
+            for (int j = i; j < n; ++j) {
+                if (Character.isLowerCase(s.charAt(j))) {
+                    lower |= 1 << (s.charAt(j) - 'a');
+                } else {
+                    upper |= 1 << (s.charAt(j) - 'A');
+                }
+                if (lower == upper && j - i + 1 > maxLen) {
+                    maxPos = i;
+                    maxLen = j - i + 1;
+                }
+            }
+        }
+        return s.substring(maxPos, maxPos + maxLen);
+    }
+
+    public String longestNiceSubstring0(String s) {
+        int n = s.length();
         String ans = "";
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
@@ -64,8 +86,20 @@ public class LongestNiceSubstring {
 
     public static void main(String[] args) {
         LongestNiceSubstring longestNiceSubstring = new LongestNiceSubstring();
-        String s = "YazaAay";
+//        String s = "YazaAay";
+        String s = "abC";
         String result = longestNiceSubstring.longestNiceSubstring(s);
         System.out.println(result);
+
+        int low = 1;
+        low = low << 1;
+        low = low << 2;
+        int high = 1;
+        high = high << 3;
+        System.out.println(low);
+        System.out.println(high);
+
+        int x = (int) ('c' - 'a');
+        System.out.println(x);
     }
 }
