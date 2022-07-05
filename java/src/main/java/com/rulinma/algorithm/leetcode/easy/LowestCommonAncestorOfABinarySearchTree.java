3d09191647228c282cb38045c653e412843ae20c
rulinma <rulinma@gmail.com>
2022-07-05 11:43:26 +0800

    235. 二叉搜索树的最近公共祖先

---
+++ java/src/main/java/com/rulinma/algorithm/leetcode/easy/LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,39 @@
+package com.rulinma.algorithm.leetcode.easy;
+
+import com.rulinma.algorithm.leetcode.common.TreeNode;
+
+/**
+ * 235. 二叉搜索树的最近公共祖先
+ * <p>
+ * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+ * <p>
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ * <p>
+ * 例如，给定如下二叉搜索树:  root = [6,2,8,0,4,7,9,null,null,3,5]
+ * <p>
+ * <p>
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * 输出: 6
+ * 解释: 节点 2 和节点 8 的最近公共祖先是 6。
+ * <p>
+ * https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-search-tree
+ *
+ * @author 马如林
+ * @Data 2022/7/5 11:42
+ */
+public class LowestCommonAncestorOfABinarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // 如果小于0，说明p和q位于root的两侧，直接返回即可
+        // 等于0，则当前root就是公共祖先
+        if ((root.val - p.val) * (root.val - q.val) <= 0) {
+            return root;
+        }
+        // 否则，p和q位于root的同一侧，就继续往下找
+        return lowestCommonAncestor(p.val < root.val ? root.left : root.right, p, q);
+    }
+}
