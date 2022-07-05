11e1f5e227216ca4eb829ed5071101e708201888
rulinma <rulinma@gmail.com>
2022-07-05 14:14:36 +0800

    98. 验证二叉搜索树

--- java/src/main/java/com/rulinma/algorithm/leetcode/middle/ValidateBinarySearchTree.java
+++ java/src/main/java/com/rulinma/algorithm/leetcode/middle/ValidateBinarySearchTree.java
@@ -3,23 +3,36 @@ package com.rulinma.algorithm.leetcode.middle;
 import com.rulinma.algorithm.leetcode.common.TreeNode;
 
 /**
+ * 98. 验证二叉搜索树
+ * <p>
+ * 给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+ * <p>
+ * 有效 二叉搜索树定义如下：
+ * <p>
+ * 节点的左子树只包含 小于 当前节点的数。
+ * 节点的右子树只包含 大于 当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * <p>
+ * https://leetcode.cn/problems/validate-binary-search-tree
+ *
  * @author 马如林
  * @Data 2022/7/5 11:51
  */
 public class ValidateBinarySearchTree {
 
     public boolean isValidBST(TreeNode root) {
-
-        if (root.left != null && root.left.val >= root.val) {
-            return false;
-        }
-
-        if (root.right != null && root.right.val <= root.val) {
-            return false;
-        }
-
-        return (root.left != null && isValidBST(root.left)) && (root.right != null && isValidBST(root.right));
-
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
+
+    public boolean isValidBST(TreeNode node, long lower, long upper) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= lower || node.val >= upper) {
+            return false;
+        }
+        return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
+    }
+
 
 }
