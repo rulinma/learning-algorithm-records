62ffe44f75942699ea3960dd99d9d1e95686911d
rulinma <rulinma@gmail.com>
2022-07-11 08:49:53 +0800

    844. 比较含退格的字符串

---
+++ java/src/main/java/com/rulinma/algorithm/leetcode/easy/BackspaceStringCompare.java
@@ -0,0 +1,104 @@
+package com.rulinma.algorithm.leetcode.easy;
+
+/**
+ * 844. 比较含退格的字符串
+ * 给定 s 和 t 两个字符串，当它们分别被输入到空白的文本编辑器后，如果两者相等，返回 true 。# 代表退格字符。
+ * <p>
+ * 注意：如果对空文本输入退格字符，文本继续为空。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "ab#c", t = "ad#c"
+ * 输出：true
+ * 解释：s 和 t 都会变成 "ac"。
+ * 示例 2：
+ * <p>
+ * 输入：s = "ab##", t = "c#d#"
+ * 输出：true
+ * 解释：s 和 t 都会变成 ""。
+ * 示例 3：
+ * <p>
+ * 输入：s = "a#c", t = "b"
+ * 输出：false
+ * 解释：s 会变成 "c"，但 t 仍然是 "b"。
+ * <p>
+ * https://leetcode.cn/problems/backspace-string-compare
+ * <p>
+ * 你可以用 O(n) 的时间复杂度和 O(1) 的空间复杂度解决该问题吗？
+ *
+ * @author 马如林
+ * @Data 2022/7/11 08:32
+ */
+public class BackspaceStringCompare {
+    public boolean backspaceCompare(String s, String t) {
+        int i = s.length() - 1, j = t.length() - 1;
+        int skipS = 0, skipT = 0;
+
+        while (i >= 0 || j >= 0) {
+            while (i >= 0) {
+                if (s.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break;
+                }
+            }
+            while (j >= 0) {
+                if (t.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+            if (i >= 0 && j >= 0) {
+                if (s.charAt(i) != t.charAt(j)) {
+                    return false;
+                }
+            } else {
+                if (i >= 0 || j >= 0) {
+                    return false;
+                }
+            }
+            i--;
+            j--;
+        }
+        return true;
+    }
+
+    public boolean backspaceCompare1(String S, String T) {
+        return build(S).equals(build(T));
+    }
+
+    public String build(String str) {
+        StringBuilder sb = new StringBuilder();
+        int length = str.length();
+        for (int i = 0; i < length; ++i) {
+            char ch = str.charAt(i);
+            if (ch != '#') {
+                sb.append(ch);
+            } else {
+                if (sb.length() > 0) {
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        BackspaceStringCompare backspaceStringCompare = new BackspaceStringCompare();
+        String s = "ab##";
+        String t = "c#d#";
+        boolean rs = backspaceStringCompare.backspaceCompare(s, t);
+        System.out.println(rs);
+    }
+}
