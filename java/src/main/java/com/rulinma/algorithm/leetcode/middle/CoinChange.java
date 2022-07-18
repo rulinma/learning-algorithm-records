d544cc166d895c35269dd93db8176c08ad67be6c
rulinma <rulinma@gmail.com>
2022-07-18 16:59:04 +0800

    322. 零钱兑换

---
+++ java/src/main/java/com/rulinma/algorithm/leetcode/middle/CoinChange.java
@@ -0,0 +1,61 @@
+package com.rulinma.algorithm.leetcode.middle;
+
+import java.util.Arrays;
+
+/**
+ * 322. 零钱兑换
+ * 给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
+ * <p>
+ * 计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。
+ * <p>
+ * 你可以认为每种硬币的数量是无限的。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：coins = [1, 2, 5], amount = 11
+ * 输出：3
+ * 解释：11 = 5 + 5 + 1
+ * 示例 2：
+ * <p>
+ * 输入：coins = [2], amount = 3
+ * 输出：-1
+ * 示例 3：
+ * <p>
+ * 输入：coins = [1], amount = 0
+ * 输出：0
+ * <p>
+ * https://leetcode.cn/problems/coin-change
+ *
+ * @author 马如林
+ * @Data 2022/7/18 10:01
+ */
+public class CoinChange {
+
+    public int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+    public static void main(String[] args) {
+        CoinChange coinChange = new CoinChange();
+//        int[] coins = new int[]{1, 2, 5};
+//        int amount = 11;
+
+        int[] coins = new int[]{2};
+        int amount = 3;
+        int rs = coinChange.coinChange(coins, amount);
+        System.out.println(rs);
+    }
+}
