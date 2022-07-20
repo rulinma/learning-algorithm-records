a63c067c73cad605574012d36214c958873257ed
rulinma <rulinma@gmail.com>
2022-07-20 18:25:43 +0800

    337. 打家劫舍 III

---
+++ java/src/main/java/com/rulinma/algorithm/leetcode/middle/HouseRobberThree.java
@@ -0,0 +1,56 @@
+package com.rulinma.algorithm.leetcode.middle;
+
+import com.rulinma.algorithm.leetcode.common.TreeNode;
+
+import java.util.HashMap;
+
+/**
+ * 337. 打家劫舍 III
+ * 小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为 root 。
+ * <p>
+ * 除了 root 之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。 如果 两个直接相连的房子在同一天晚上被打劫 ，房屋将自动报警。
+ * <p>
+ * 给定二叉树的 root 。返回 在不触动警报的情况下 ，小偷能够盗取的最高金额 。
+ * <p>
+ * 示例 1:
+ * <p>
+ * <p>
+ * <p>
+ * 输入: root = [3,2,3,null,3,null,1]
+ * 输出: 7
+ * 解释: 小偷一晚能够盗取的最高金额 3 + 3 + 1 = 7
+ * 示例 2:
+ * <p>
+ * <p>
+ * 输入: root = [3,4,5,1,3,null,1]
+ * 输出: 9
+ * 解释: 小偷一晚能够盗取的最高金额 4 + 5 = 9
+ * <p>
+ * https://leetcode.cn/problems/house-robber-iii
+ *
+ * @author 马如林
+ * @Data 2022/7/20 18:24
+ */
+public class HouseRobberThree {
+    public int rob(TreeNode root) {
+        HashMap<TreeNode, Integer> memo = new HashMap<>();
+        return robInternal(root, memo);
+    }
+
+    public int robInternal(TreeNode root, HashMap<TreeNode, Integer> memo) {
+        if (root == null) return 0;
+        if (memo.containsKey(root)) return memo.get(root);
+        int money = root.val;
+
+        if (root.left != null) {
+            money += (robInternal(root.left.left, memo) + robInternal(root.left.right, memo));
+        }
+        if (root.right != null) {
+            money += (robInternal(root.right.left, memo) + robInternal(root.right.right, memo));
+        }
+        int result = Math.max(money, robInternal(root.left, memo) + robInternal(root.right, memo));
+        memo.put(root, result);
+        return result;
+    }
+
+}
