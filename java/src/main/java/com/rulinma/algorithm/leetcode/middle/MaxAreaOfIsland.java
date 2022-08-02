dadee159eb01785dd69907582efd28224810dda8
rulinma <rulinma@gmail.com>
2022-08-02 21:35:48 +0800

    695. 岛屿的最大面积

---
+++ java/src/main/java/com/rulinma/algorithm/leetcode/middle/MaxAreaOfIsland.java
@@ -0,0 +1,65 @@
+package com.rulinma.algorithm.leetcode.middle;
+
+/**
+ * 695. 岛屿的最大面积
+ * <p>
+ * 给你一个大小为 m x n 的二进制矩阵 grid 。
+ * <p>
+ * 岛屿 是由一些相邻的 1 (代表土地) 构成的组合，这里的「相邻」要求两个 1 必须在 水平或者竖直的四个方向上 相邻。你可以假设 grid 的四个边缘都被 0（代表水）包围着。
+ * <p>
+ * 岛屿的面积是岛上值为 1 的单元格的数目。
+ * <p>
+ * 计算并返回 grid 中最大的岛屿面积。如果没有岛屿，则返回面积为 0 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+ * 输出：6
+ * 解释：答案不应该是 11 ，因为岛屿只能包含水平或垂直这四个方向上的 1 。
+ * 示例 2：
+ * <p>
+ * 输入：grid = [[0,0,0,0,0,0,0,0]]
+ * 输出：0
+ * <p>
+ * https://leetcode.cn/problems/max-area-of-island
+ *
+ * @author 马如林
+ * @Data 2022/8/2 21:34
+ */
+public class MaxAreaOfIsland {
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    res = Math.max(res, dfs(i, j, grid));
+                }
+            }
+        }
+        return res;
+    }
+
+    private int dfs(int i, int j, int[][] grid) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == 0) {
+            return 0;
+        }
+
+        if (grid[i][j] == 2) {
+            return 0;
+        }
+
+        // 访问过
+        grid[i][j] = 2;
+        int num = 1;
+        num += dfs(i + 1, j, grid);
+        num += dfs(i - 1, j, grid);
+        num += dfs(i, j + 1, grid);
+        num += dfs(i, j - 1, grid);
+        return num;
+    }
+
+}
