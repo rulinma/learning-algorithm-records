adb412faa30680485132460edc62fc5eb54cda86
rulinma <rulinma@gmail.com>
2022-08-10 21:22:31 +0800

    归并排序

--- java/src/main/java/com/rulinma/algorithm/leetcode/middle/SortAnArray.java
+++ java/src/main/java/com/rulinma/algorithm/leetcode/middle/SortAnArray.java
@@ -16,6 +16,56 @@ import java.util.Arrays;
 @Slf4j
 public class SortAnArray {
 
+    public void mergeSort(int[] arr) {
+        int[] temp = new int[arr.length];
+        // 在排序前，先建好一个长度等于原数组长度的临时数组，避免递归中频繁开辟空间
+        sort(arr, 0, arr.length - 1, temp);
+    }
+
+    private void sort(int[] arr, int left, int right, int[] temp) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            // 左边归并排序，使得左子序列有序
+            sort(arr, left, mid, temp);
+            // 右边归并排序，使得右子序列有序
+            sort(arr, mid + 1, right, temp);
+            // 将两个有序子数组合并操作
+            merge(arr, left, mid, right, temp);
+        }
+    }
+
+    private void merge(int[] arr, int left, int mid, int right, int[] temp) {
+        // 左序列指针
+        int i = left;
+        // 右序列指针
+        int j = mid + 1;
+        // 临时数组指针
+        int t = 0;
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[t++] = arr[i++];
+            } else {
+                temp[t++] = arr[j++];
+            }
+        }
+
+        // 将左边剩余元素填充进temp中
+        while (i <= mid) {
+            temp[t++] = arr[i++];
+        }
+
+        // 将右序列剩余元素填充进temp中
+        while (j <= right) {
+            temp[t++] = arr[j++];
+        }
+
+        t = 0;
+        // 将temp中的元素全部拷贝到原数组中
+        while (left <= right) {
+            arr[left++] = temp[t++];
+        }
+    }
+
     public void swaps(int[] nums, int index, int min) {
         int temp = nums[index];
         nums[index] = nums[min];
@@ -99,10 +149,14 @@ public class SortAnArray {
 
     public static void main(String[] args) {
         SortAnArray sortAnArray = new SortAnArray();
-        int[] nums = new int[]{11, 30, 2, 4, 15, 60, 1, 3, 5, 8};
-//        int[] nums = new int[]{11, 30, 2, 4, 15, 60, 1};
-        int[] rs = sortAnArray.sortArrayByHeap(nums);
-        System.out.println(Arrays.toString(rs));
+//        int[] nums = new int[]{11, 30, 2, 4, 15, 60, 1, 3, 5, 8};
+////        int[] nums = new int[]{11, 30, 2, 4, 15, 60, 1};
+//        int[] rs = sortAnArray.sortArrayByHeap(nums);
+//        System.out.println(Arrays.toString(rs));
+
+        int[] arr = {9, 8, 17, 6, 5, 4, 13, 2, 1};
+        sortAnArray.mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
     /**
