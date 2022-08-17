27e231b0569eac06fe20c3fd61d6259b9e52e982
rulinma <rulinma@gmail.com>
2022-08-17 09:54:25 +0800

    1302. 层数最深叶子节点的和

---
+++ java/src/main/java/com/rulinma/algorithm/leetcode/middle/DeepestLeavesSum.java
@@ -0,0 +1,93 @@
+package com.rulinma.algorithm.leetcode.middle;
+
+import com.rulinma.algorithm.leetcode.common.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 1302. 层数最深叶子节点的和
+ * <p>
+ * 给你一棵二叉树的根节点 root ，请你返回 层数最深的叶子节点的和 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：root = [1,2,3,4,5,null,6,7,null,null,null,null,8]
+ * 输出：15
+ * 示例 2：
+ * <p>
+ * 输入：root = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]
+ * 输出：19
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 树中节点数目在范围 [1, 104] 之间。
+ * 1 <= Node.val <= 100
+ * <p>
+ * https://leetcode.cn/problems/deepest-leaves-sum
+ *
+ * @author 马如林
+ * @Data 2022/8/17 09:26
+ */
+public class DeepestLeavesSum {
+
+    public int deepestLeavesSum(TreeNode root) {
+        int rs = 0;
+
+        // 遍历到最后一层，统计值
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            // 统计每一层的和
+            rs = 0;
+            // 添加下一层
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode treeNode = queue.poll();
+                rs += treeNode.val;
+                if (treeNode.left != null) {
+                    queue.add(treeNode.left);
+                }
+                if (treeNode.right != null) {
+                    queue.add(treeNode.right);
+                }
+            }
+        }
+
+        return rs;
+    }
+
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        TreeNode t4 = new TreeNode(4);
+        TreeNode t5 = new TreeNode(5);
+        TreeNode t6 = new TreeNode(6);
+        TreeNode t7 = new TreeNode(7);
+        TreeNode t8 = new TreeNode(8);
+
+        t1.left = t2;
+        t1.right = t3;
+
+        t2.left = t4;
+        t2.right = t5;
+
+        t3.right = t6;
+
+        t4.left = t7;
+
+        t6.right = t8;
+
+
+        DeepestLeavesSum deepestLeavesSum = new DeepestLeavesSum();
+
+        System.out.println(deepestLeavesSum.deepestLeavesSum(t1));
+
+    }
+}
