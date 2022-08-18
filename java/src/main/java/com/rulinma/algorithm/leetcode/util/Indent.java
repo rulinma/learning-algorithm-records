4f48b1aaea96e53ef009ed9da4db0eb5422712ff
rulinma <rulinma@gmail.com>
2022-08-18 11:02:30 +0800

    缩进工具

---
+++ java/src/main/java/com/rulinma/algorithm/leetcode/util/Indent.java
@@ -0,0 +1,15 @@
+package com.rulinma.algorithm.leetcode.util;
+
+/**
+ * @author 马如林
+ * @Data 2022/8/18 11:01
+ */
+public class Indent {
+
+    public static void printIndent(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(("   ");
+        }
+    }
+
+}
