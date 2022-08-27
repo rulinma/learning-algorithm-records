ddfada648ff4df9acc27161bdd846f3b3ceefd37
rulinma <rulinma@gmail.com>
2022-08-27 09:56:26 +0800

    662. 二叉树最大宽度

--- java/src/main/java/com/rulinma/algorithm/leetcode/middle/MaximumWidthOfBinaryTree.java
+++ java/src/main/java/com/rulinma/algorithm/leetcode/middle/MaximumWidthOfBinaryTree.java
@@ -1,6 +1,7 @@
 package com.rulinma.algorithm.leetcode.middle;
 
 import com.rulinma.algorithm.leetcode.common.TreeNode;
+import javafx.util.Pair;
 
 import java.util.*;
 
@@ -47,6 +48,28 @@ import java.util.*;
  * @Data 2022/8/27 07:53
  */
 public class MaximumWidthOfBinaryTree {
+
+    public int widthOfBinaryTree1(TreeNode root) {
+        int res = 1;
+        List<Pair<TreeNode, Integer>> arr = new ArrayList<Pair<TreeNode, Integer>>();
+        arr.add(new Pair<TreeNode, Integer>(root, 1));
+        while (!arr.isEmpty()) {
+            List<Pair<TreeNode, Integer>> tmp = new ArrayList<Pair<TreeNode, Integer>>();
+            for (Pair<TreeNode, Integer> pair : arr) {
+                TreeNode node = pair.getKey();
+                int index = pair.getValue();
+                if (node.left != null) {
+                    tmp.add(new Pair<TreeNode, Integer>(node.left, index * 2));
+                }
+                if (node.right != null) {
+                    tmp.add(new Pair<TreeNode, Integer>(node.right, index * 2 + 1));
+                }
+            }
+            res = Math.max(res, arr.get(arr.size() - 1).getValue() - arr.get(0).getValue() + 1);
+            arr = tmp;
+        }
+        return res;
+    }
 
     /**
      * 暴力解法
