e1dd60beb2f164079a704968a31f8ecc771a67ca
rulinma <rulinma@gmail.com>
2022-08-31 19:21:41 +0800

    1535. 找出数组游戏的赢家

--- java/src/main/java/com/rulinma/algorithm/leetcode/match/weekly/Test200.java
+++ java/src/main/java/com/rulinma/algorithm/leetcode/match/weekly/Test200.java
@@ -7,10 +7,56 @@ package com.rulinma.algorithm.leetcode.match.weekly;
 public class Test200 {
 
     public int getWinner(int[] arr, int k) {
-        int rs = 0;
+        // 算法
+        // 1. 统计当前位置最大值
+        // 2. 统计当前位置的后续连续位置比我小的值得个数
+        // 2.1 当前值 + 后续值 == k 则返回，否则继续后移
+        // 3. 第一遍没找到则必然是最大值
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+        }
 
+        int[] maxVal = new int[arr.length];
+        int currentMax = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > currentMax) {
+                currentMax = arr[i];
+            }
+            maxVal[i] = currentMax;
+        }
 
-        return rs;
+        int i = 0;
+        while (i < arr.length) {
+            // 开始暴力破解
+            if (arr[i] == maxVal[i]) {
+                // 需要后续k-1个值小
+                int c = 0;
+                if (i != 0) {
+                    c = 1;
+                }
+                if (c == k) {
+                    // get it
+                    return arr[i];
+                }
+
+                int j = 1;
+                while (i + j < arr.length && arr[i + j] < arr[i]) {
+                    c++;
+                    if (c == k) {
+                        // get it
+                        return arr[i];
+                    }
+                    j++;
+                }
+                // skip
+                i += j;
+            } else {
+                i++;
+            }
+        }
+
+        return max;
     }
 
     /**
