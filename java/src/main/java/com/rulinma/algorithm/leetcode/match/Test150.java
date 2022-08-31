4d8dd319b28ded25ddd2997956268a310e9641e1
rulinma <rulinma@gmail.com>
2022-08-31 11:55:07 +0800

    1160. 拼写单词

--- java/src/main/java/com/rulinma/algorithm/leetcode/match/Test150.java
+++ java/src/main/java/com/rulinma/algorithm/leetcode/match/Test150.java
@@ -12,10 +12,42 @@ import java.util.Map;
  */
 public class Test150 {
 
+    public int countCharacters(String[] words, String chars) {
+        int[] chars_count = count(chars); // 统计字母表的字母出现次数
+        int res = 0;
+        for (String word : words) {
+            int[] word_count = count(word); // 统计单词的字母出现次数
+            if (contains(chars_count, word_count)) {
+                res += word.length();
+            }
+        }
+        return res;
+    }
+
+    // 检查字母表的字母出现次数是否覆盖单词的字母出现次数
+    boolean contains(int[] chars_count, int[] word_count) {
+        for (int i = 0; i < 26; i++) {
+            if (chars_count[i] < word_count[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 统计 26 个字母出现的次数
+    int[] count(String word) {
+        int[] counter = new int[26];
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            counter[c - 'a']++;
+        }
+        return counter;
+    }
+
     /**
      * 1160. 拼写单词
      */
-    public int countCharacters(String[] words, String chars) {
+    public int countCharacters1(String[] words, String chars) {
         int rs = 0;
         Map<Character, Integer> map = CharCount(chars);
         // 检查每个word能否用chars表示出来
