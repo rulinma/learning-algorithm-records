a6398fa4e85b7264e5cafc520bfc7a827ac24a64
rulinma <rulinma@gmail.com>
2022-08-31 17:43:42 +0800

    1534. 统计好三元组

---
+++ java/src/main/java/com/rulinma/algorithm/leetcode/match/Test200.java
@@ -0,0 +1,35 @@
+package com.rulinma.algorithm.leetcode.match;
+
+/**
+ * @author 马如林
+ * @Data 2022/8/31 11:14
+ */
+public class Test200 {
+
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int rs = 0;
+        // 0 <= i < j < k < arr.length
+        for (int i = 0; i < arr.length - 2; i++) {
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                if (Math.abs(arr[i] - arr[j]) > a) {
+                    continue;
+                }
+                for (int k = j + 1; k < arr.length; k++) {
+                    if (Math.abs(arr[i] - arr[j]) <= a &&
+                            Math.abs(arr[j] - arr[k]) <= b &&
+                            Math.abs(arr[i] - arr[k]) <= c
+                    ) {
+                        rs++;
+                    }
+                }
+            }
+        }
+
+        return rs;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
