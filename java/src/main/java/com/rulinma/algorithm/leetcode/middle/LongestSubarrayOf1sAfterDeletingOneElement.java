45e7b3abb626d02aaf4f84d742e8f89690a55888
rulinma <rulinma@gmail.com>
2022-09-09 12:01:49 +0800

    1493. 删掉一个元素以后全为 1 的最长子数组

--- java/src/main/java/com/rulinma/algorithm/leetcode/middle/LongestSubarrayOf1sAfterDeletingOneElement.java
+++ java/src/main/java/com/rulinma/algorithm/leetcode/middle/LongestSubarrayOf1sAfterDeletingOneElement.java
@@ -1,11 +1,43 @@
 package com.rulinma.algorithm.leetcode.middle;
 
-import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.List;
 
 /**
+ * 1493. 删掉一个元素以后全为 1 的最长子数组
+ * <p>
+ * 给你一个二进制数组 nums ，你需要从中删掉一个元素。
+ * <p>
+ * 请你在删掉元素的结果数组中，返回最长的且只包含 1 的非空子数组的长度。
+ * <p>
+ * 如果不存在这样的子数组，请返回 0 。
+ * <p>
+ *  
+ * <p>
+ * 提示 1：
+ * <p>
+ * 输入：nums = [1,1,0,1]
+ * 输出：3
+ * 解释：删掉位置 2 的数后，[1,1,1] 包含 3 个 1 。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [0,1,1,1,0,1,1,0,1]
+ * 输出：5
+ * 解释：删掉位置 4 的数字后，[0,1,1,1,1,1,0,1] 的最长全 1 子数组为 [1,1,1,1,1] 。
+ * 示例 3：
+ * <p>
+ * 输入：nums = [1,1,1]
+ * 输出：2
+ * 解释：你必须要删除一个元素。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 105
+ * nums[i] 要么是 0 要么是 1 。
+ * <p>
+ * https://leetcode.cn/problems/longest-subarray-of-1s-after-deleting-one-element
+ *
  * @author 马如林
  * @Data 2022/9/9 11:38
  */
