7d3cea93a22c2f914b4d74fd3e1b197d58386ac3
rulinma <rulinma@gmail.com>
2022-09-19 18:04:23 +0800

    2320. 统计放置房子的方式数

--- java/src/main/java/com/rulinma/algorithm/leetcode/match/weekly/doubleweek/Test299.java
+++ java/src/main/java/com/rulinma/algorithm/leetcode/match/weekly/doubleweek/Test299.java
@@ -6,6 +6,29 @@ package com.rulinma.algorithm.leetcode.match.weekly.doubleweek;
  */
 public class Test299 {
 
+    public int countHousePlacements(int n) {
+        int MOD = (int) 1e9 + 7;
+        int MX = (int) 1e4 + 1;
+        int[] f = new int[n + 1];
+        if (n == 1) {
+            return 4;
+        }
+        if (n == 2) {
+            return 9;
+        }
+        // 定义 f[i] 表示前 i 个地块的放置方案数
+        // 放1，不放
+        f[1] = 2;
+        // 放1，放2，都不放
+        f[2] = 3;
+        for (int i = 3; i <= n; ++i) {
+            f[i] = (f[i - 1] + f[i - 2]) % MOD;
+        }
+
+        return (int) ((long) f[n] * f[n] % MOD);
+    }
+
+
     public boolean checkXMatrix(int[][] grid) {
         boolean ans = true;
         int len = grid.length;
