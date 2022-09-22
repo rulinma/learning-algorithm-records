c863104c20a346f0ae4a250715b712399e6291cc
rulinma <rulinma@gmail.com>
2022-09-22 17:56:13 +0800

    347. 前 K 个高频元素

--- java/src/main/java/interview/Test.java
+++ java/src/main/java/interview/Test.java
@@ -1,17 +1,42 @@
 package interview;
 
 import javafx.util.Pair;
+import org.omg.PortableInterceptor.INACTIVE;
 
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * @author 马如林
  * @Data 2022/8/29 10:25
  */
 public class Test {
+
+    public int[] topKFrequent(int[] nums, int k) {
+        int[] ans = new int[k];
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            int count = map.getOrDefault(n, 0);
+            map.put(n, count + 1);
+        }
+
+        PriorityQueue<Pair<Integer, Integer>> priorityQueue = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        Iterator iterator = map.keySet().iterator();
+        while (iterator.hasNext()) {
+            Integer key = (Integer) iterator.next();
+            Integer value = (Integer) map.get(key);
+            Pair<Integer, Integer> pair = new Pair<>(key, value);
+            priorityQueue.add(pair);
+        }
+
+        int j = 0;
+        for (int i = 0; i < k; i++) {
+            ans[j++] = priorityQueue.poll().getKey();
+        }
+
+        return ans;
+    }
+
 
     public int[] dailyTemperatures(int[] temperatures) {
         // 单调栈
@@ -91,9 +116,17 @@ public class Test {
 
     public static void main(String[] args) {
         Test test = new Test();
-        int[] t = new int[]{73, 74, 75, 71, 69, 72, 76, 73};
-        int[] rs = test.dailyTemperatures(t);
+//        int[] t = new int[]{73, 74, 75, 71, 69, 72, 76, 73};
+//        int[] rs = test.dailyTemperatures(t);
+//        System.out.println(Arrays.toString(rs));
+
+        int[] t = new int[]{1, 1, 1, 2, 2, 3};
+        int[] rs = test.topKFrequent(t, 2);
         System.out.println(Arrays.toString(rs));
+
+        int[] t1 = new int[]{1};
+        int[] rs1 = test.topKFrequent(t1, 1);
+        System.out.println(Arrays.toString(rs1));
     }
 
 }
