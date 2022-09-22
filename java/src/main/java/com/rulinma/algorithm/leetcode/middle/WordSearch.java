14499dcf26aec125a7c8755b61923b80ec01c5dd
rulinma <rulinma@gmail.com>
2022-09-22 21:10:49 +0800

    62. 不同路径

---
+++ java/src/main/java/com/rulinma/algorithm/leetcode/middle/WordSearch.java
@@ -0,0 +1,13 @@
+package com.rulinma.algorithm.leetcode.middle;
+
+/**
+ * @author 马如林
+ * @Data 2022/9/22 21:10
+ */
+public class WordSearch {
+    public boolean exist(char[][] board, String word) {
+        // dfs
+
+        return true;
+    }
+}
