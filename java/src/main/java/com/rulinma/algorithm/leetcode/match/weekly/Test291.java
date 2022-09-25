5196b978070f7bc1d650f10ed3b45e3b0898de55
rulinma <rulinma@gmail.com>
2022-09-25 19:21:47 +0800

    2259. 移除指定数字得到的最大结果

--- java/src/main/java/com/rulinma/algorithm/leetcode/match/weekly/Test291.java
+++ java/src/main/java/com/rulinma/algorithm/leetcode/match/weekly/Test291.java
@@ -1,10 +1,43 @@
 package com.rulinma.algorithm.leetcode.match.weekly;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author 马如林
  * @Data 2022/9/25 18:18
  */
 public class Test291 {
+
+    /**
+     * 2260. 必须拿起的最小连续卡牌数
+     */
+    public int minimumCardPickup(int[] cards) {
+        int ans = 10000000;
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] f = new int[cards.length];
+        Arrays.fill(f, 10000000);
+
+        for (int i = 0; i < cards.length; i++) {
+            Integer index = map.get(cards[i]);
+            if (index != null) {
+                f[i] = i - index;
+            }
+            map.put(cards[i], i);
+        }
+
+        for (int i : f) {
+            ans = Math.min(i, ans);
+        }
+
+        if (ans == 0) {
+            return -1;
+        }
+
+        return ans + 1;
+    }
+
 
     /**
      * 2259. 移除指定数字得到的最大结果
@@ -28,5 +61,12 @@ public class Test291 {
         return max;
     }
 
+
+    public static void main(String[] args) {
+        Test291 test291 = new Test291();
+        int[] cards = new int[]{3, 4, 2, 3, 4, 7};
+        int rs = test291.minimumCardPickup(cards);
+        System.out.println(rs);
+    }
 
 }
