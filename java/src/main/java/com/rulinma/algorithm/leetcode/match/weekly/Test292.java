5c1d841ec04bd40c755f6037e205b027498a96e8
rulinma <rulinma@gmail.com>
2022-09-26 10:23:44 +0800

    2265. 统计值等于子树平均值的节点数

--- java/src/main/java/com/rulinma/algorithm/leetcode/match/weekly/Test292.java
+++ java/src/main/java/com/rulinma/algorithm/leetcode/match/weekly/Test292.java
@@ -1,10 +1,43 @@
 package com.rulinma.algorithm.leetcode.match.weekly;
 
+import com.rulinma.algorithm.leetcode.common.TreeNode;
+
 /**
  * @author 马如林
  * @Data 2022/9/26 09:25
  */
 public class Test292 {
+
+    int count = 0;
+
+    /**
+     * 2265. 统计值等于子树平均值的节点数
+     */
+    public int averageOfSubtree(TreeNode root) {
+        dfs(root, 0);
+
+        return count;
+    }
+
+    public int[] dfs(TreeNode node, Integer c) {
+        if (node == null) {
+            return new int[]{0, 0};
+        }
+
+
+        int[] left = dfs(node.left, c);
+        int[] right = dfs(node.right, c);
+
+
+        int sum = left[0] + right[0] + node.val;
+
+        int nodes = left[1] + right[1] + 1;
+        if (sum / nodes == node.val) {
+            count++;
+        }
+
+        return new int[]{sum, nodes};
+    }
 
     /**
      * 2264. 字符串中最大的 3 位相同数字
